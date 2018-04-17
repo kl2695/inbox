f5f3a74db5b41cce93d152daf17f29fab152bb10
Kevin <kl2695@nyu.edu>
2018-04-17 09:25:15 -0300

    write tests for contract that it deploys, has a default message, and can change a message

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,12 +1,16 @@
+
+
 const assert = require('assert'); 
 const ganache = require('ganache-cli'); 
 const Web3 = require('web3'); 
-const web3 = new Web3(ganache.provider()); 
+const provider = ganache.provider(); 
+const web3 = new Web3(provider); 
 const { interface, bytecode } = require('../compile'); 
 
 
 let accounts;
 let inbox;  
+const INITIAL_STRING = 'Hi there'; 
 
 beforeEach(async () => {
     // Get a list of all accounts
@@ -16,13 +20,29 @@ beforeEach(async () => {
     //use one of those accounts to deploy the contract 
 
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['Hi there'] })
+        .deploy({ data: bytecode, arguments: [INITIAL_STRING] })
         .send({ from: accounts[0], gas: '1000000'})
+
+    inbox.setProvider(provider); 
 }); 
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(inbox);
-        
+        assert.ok(inbox.options.address);
+        console.log(inbox.options.address);
     }); 
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call(); 
+        assert.equal(message, INITIAL_STRING); 
+        console.log(message);
+        
+    });
+
+    it('can change the message', async () => {
+        await inbox.methods.setMessage('bye')
+        .send({ from: accounts[0], gas: '1000000'});
+        const message = await inbox.methods.message().call(); 
+        assert.equal(message, 'bye'); 
+    })
 }); 
