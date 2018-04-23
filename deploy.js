b6632d84755d1b2cb13a2deffb0abf040e808f5a
kevin lee <kl2695@nyu.edu>
2018-04-23 15:27:21 -0300

    contract deployed to rinkeby testnet

---
+++ deploy.js
@@ -0,0 +1,28 @@
+const HDWalletProvider = require('truffle-hdwallet-provider'); 
+const Web3 = require('web3') 
+const { interface, bytecode } = require('./compile'); 
+
+const provider = new HDWalletProvider(
+    'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat', 
+    'https://rinkeby.infura.io/1jSHr7QaeTYsHnWP76TE'
+);
+
+const web3 = new Web3(provider);
+
+
+let inbox; 
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts(); 
+
+    console.log(accounts);
+    console.log('Attempting to deploy from account', accounts[0]); 
+
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!']})
+        .send({ gas: '1000000', from: accounts[0]});
+
+    console.log('Contract deployed to', inbox.options.address);
+
+
+};
+deploy(); 
