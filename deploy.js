9d15f317a317fef0abdc2775391f5d3af902ca6f
kevin lee <kl2695@nyu.edu>
2018-04-23 15:42:48 -0300

    deploy to rinkeby

--- deploy.js
+++ deploy.js
@@ -5,7 +5,7 @@ const { interface, bytecode } = require('./compile');
 const provider = new HDWalletProvider(
     'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat', 
     'https://rinkeby.infura.io/1jSHr7QaeTYsHnWP76TE'
-);
+); 
 
 const web3 = new Web3(provider);
 
